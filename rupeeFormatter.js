2a10d169ff84ed958386f9fa2e8edddcb7fdeda9
Chetan Sisodiya <chetan.sisodiya@gmail.com>
2017-06-17 01:11:35 +0530

    rupee format code

---
+++ rupeeFormatter.js
@@ -0,0 +1,16 @@
+module.exports = function rupeeFormatter(amount){
+  if(amount === ""){
+    return "₹ -";
+  }
+  var amountArr = amount.split(".");
+  var first = amountArr[0];
+  var decimal = amountArr[1];
+  var firstdigit = first.split("").pop();
+  var lastPart = first.split("").filter((v, i , A)=>{return i!== A.length-1}).join("");
+  var lastPartFormatted = lastPart.split("").reverse().map((v, i, A)=>{if(i!==A.length-1){return `${A[i+1]}${v}`}else{return v}}).filter((v, i)=>{return i%2 === 0}).reverse().join(",");
+  if(decimal !== undefined){
+    return `₹ ${lastPartFormatted}${firstdigit}.${decimal}`;
+  } else {
+    return `₹ ${lastPartFormatted}${firstdigit}`;
+  }
+}
---
+++ test/rupeeFormatter.test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+import rupeeFormatter from "../rupeeFormatter";
+
+describe('Testing', function() {
+  it('All test case must passing', function() {
+    assert.equal(rupeeFormatter("1"), "₹ 1");
+    assert.equal(rupeeFormatter("100"), "₹ 100");
+    assert.equal(rupeeFormatter("1400"), "₹ 1,400");
+    assert.equal(rupeeFormatter("1400.67"), "₹ 1,400.67");
+    assert.equal(rupeeFormatter("14000.67"), "₹ 14,000.67");
+    assert.equal(rupeeFormatter("14000"), "₹ 14,000");
+    assert.equal(rupeeFormatter("174000"), "₹ 1,74,000");
+    assert.equal(rupeeFormatter("1753676545643.345434"), "₹ 17,53,67,65,45,643.345434");
+    assert.equal(rupeeFormatter(""), "₹ -");
+  });
+});
